95c409a0f154c2a79658fcea2b81280a54a81cca
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-03-03 11:15:31 -0800

    wrote tests

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,6 @@
 import hashlib
 
-def crack_sha1_hash(hash):
+def crack_sha1_hash(hash, use_salts=None):
+    # return the password if it is in the top 10000
+    # else return "PASSWORD NOT IN DATABASE"
     pass
