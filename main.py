aeeb2ad354f1522dc78d67f15dbf6ce648e0bea0
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-03-03 12:51:28 -0800

    oh god what the fuck did i do

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from unittest import main
 #     "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
 # print(cracked_password1)
 
-print(password_cracker.crack_sha1_hash("b305921a3723cd5d70a375cd21a61e60aabb84ec")) # sammy123
-print(password_cracker.crack_sha1_hash("c7ab388a5ebefbf4d550652f1eb4d833e5316e3e")) # abacab
-print(password_cracker.crack_sha1_hash("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")) # password
+# print(password_cracker.crack_sha1_hash("b305921a3723cd5d70a375cd21a61e60aabb84ec")) # sammy123
+# print(password_cracker.crack_sha1_hash("c7ab388a5ebefbf4d550652f1eb4d833e5316e3e")) # abacab
+# print(password_cracker.crack_sha1_hash("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")) # password
 
-# print(password_cracker.crack_sha1_hash("53d8b3dc9d39f0184144674e310185e41a87ffd5", True)) # superman
+print(password_cracker.crack_sha1_hash("53d8b3dc9d39f0184144674e310185e41a87ffd5", True)) # superman
 # print(password_cracker.crack_sha1_hash("da5a4e8cf89539e66097acd2f8af128acae2f8ae", True)) # q1w2e3r4t5
 # print(password_cracker.crack_sha1_hash("ea3f62d498e3b98557f9f9cd0d905028b3b019e1", True)) # bubbles1
 
--- password_cracker.py
+++ password_cracker.py
@@ -2,16 +2,26 @@ import hashlib
 
 def crack_sha1_hash(hash, use_salts=None):
     try:
-        sha1_file = open('top-10000-passwords.txt', 'r')
+        sha1_file = open('top-10000-passwords.txt', 'r').readlines()
+        known_salts = open('known-salts.txt', 'r').readlines()
     except:
         return "Error: File not found"
-    # return the password if it is in the top 10000
-    # else return "PASSWORD NOT IN DATABASE"
     
-    for password in sha1_file.readlines():
-        hash_obj = hashlib.sha1(password.strip().encode('utf-8')).hexdigest()
-        if hash_obj == hash:
-            return str(password)
-        
+    if not use_salts == True:
+        for password in sha1_file.readlines():
+            hash_obj = hashlib.sha1(password.strip().encode('utf-8')).hexdigest()
+            if hash_obj == hash:
+                return str(password)
+    
+    else:
+        for password in sha1_file:
+            password = password.strip().encode('utf-8')
+            for salt in known_salts:
+                salt = salt.strip().encode('utf-8')
+                hash_obj = hashlib.pbkdf2_hmac('sha1', password=password, salt=salt, iterations=1000).hex()
+                print(hash_obj)
+                if hash_obj == hash:
+                    return str(password) 
+                   
     return "PASSWORD NOT IN DATABASE"
    
