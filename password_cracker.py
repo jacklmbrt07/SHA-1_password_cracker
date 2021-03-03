d85fce4f5de2f6f550712d1c3a03d413123a3780
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-03-03 11:47:28 -0800

    passes first tests

--- password_cracker.py
+++ password_cracker.py
@@ -3,17 +3,15 @@ import hashlib
 def crack_sha1_hash(hash, use_salts=None):
     try:
         sha1_file = open('top-10000-passwords.txt', 'r')
-        print(sha1_file)
     except:
-        return "Error File not found"
+        return "Error: File not found"
     # return the password if it is in the top 10000
     # else return "PASSWORD NOT IN DATABASE"
     
     for password in sha1_file.readlines():
         hash_obj = hashlib.sha1(password.strip().encode('utf-8')).hexdigest()
         if hash_obj == hash:
-            print("Password is: ", password)
-            
-    print(hashlib.algorithms_guaranteed)
-    print
-    pass
+            return str(password)
+        
+    return "PASSWORD NOT IN DATABASE"
+   
